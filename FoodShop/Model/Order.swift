3b22f19d5902f2becd06f40a45344f074960fed3
Minh Tiến <nguyenminhtien@example.com>
2021-06-25 21:25:18 +0700

    first commit

---
+++ FoodShop/Model/Order.swift
@@ -0,0 +1,16 @@
+//
+//  Order.swift
+//  FoodShop
+//
+//  Created by Nguyễn Thắng on 6/20/21.
+//  Copyright © 2021 Nguyễn Thắng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+//dối tượng đơn hàng
+struct Order {
+    let id: String?
+    let name: String?
+    let dish: Dish?
+}
